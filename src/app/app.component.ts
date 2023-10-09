eda1f054081165c385a98cbf1af89255598dcf4c
Charl913 <charlkruger61@gmail.com>
2023-10-09 10:54:34 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'assignment-two';
+  userName ='';
+
+  clearInput() {
+    return this.userName = '';
+  }
 }
